ee0f295ac35b500bb104ce11baabb44ff5155b4e
Garry Sh <sooo.lame@gmail.com>
2019-05-13 00:21:51 +1000

    added some functionality

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
+const isFieldValid = field => field.search(/^[a-h][1-8]$/) === 0;
+
+const firstLetterKeyCode = 97;
+
+const fieldToPair = field => {
+  const x = field.charCodeAt(0) - firstLetterKeyCode;
+  const y = Number(field[1]) - 1;
+  return [x, y];
+};
+
+const pairToField = pair => {
+  const [x, y] = pair;
+  const firstLetter = String.fromCharCode(x + firstLetterKeyCode);
+  const secondLetter = (y + 1).toString();
+  return `${firstLetter}${secondLetter}`;
+};
+
+const getPosibleMovies = pair => {
+  const boardSize = 7;
+  // массив всех ходов
+  const allMovies = [[1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]];
+  const movies = allMovies.map(([x, y]) => [x + pair[0], y + pair[1]]);
+  // оставляем возможные ходы
+  const posibleMovies = movies.filter(([x, y]) => x > 0 && x < boardSize && y > 0 && y < boardSize);
+  return posibleMovies;
+};
+
 export default () => {};
